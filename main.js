b06b75bbc4c19fece16d7c64e26def7d8e753774
tiffam <alesialim@gmail.com>
2018-08-28 17:53:53 +0800

    Add files via upload
    Work done on windows

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ M.Slider.init(slider, {
     height: 500,
     transition: 500,
     interval: 6000
-  });
+});
 
 // Scrollspy
 const ss = document.querySelectorAll('.scrollspy');
@@ -25,11 +25,11 @@ M.Autocomplete.init(ac, {
     data: {
 
     }
-  });
+});
 
-  function openTab(evt, cityName) {
+function openTab(evt, cityName) {
     // Declare all variables
-    var i, tabcontent, tablinks;
+    let i, tabcontent, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -47,3 +47,65 @@ M.Autocomplete.init(ac, {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+function openNav() {
+    if (document.getElementById("promotions-layer").className.includes("hidden") === false) {
+        document.getElementById("promotions-layer").classList.add("hidden");
+    }
+    if (document.getElementById("terms-layer").className.includes("hidden") === false) {
+        document.getElementById("terms-layer").classList.add("hidden");
+    }
+    let contact = document.getElementById("contact-layer")
+    contact.className = contact.className.replace("hidden", "");
+}
+
+function openNavTerm() {
+    if (document.getElementById("promotions-layer").className.includes("hidden") === false) {
+        document.getElementById("promotions-layer").classList.add("hidden");
+    }
+
+    if (document.getElementById("contact-layer").className.includes("hidden") === false) {
+        document.getElementById("contact-layer").classList.add("hidden");
+    }
+
+    let terms = document.getElementById("terms-layer")
+    terms.className = terms.className.replace("hidden", "");
+}
+
+function openNavPromo() {
+    if (document.getElementById("contact-layer").className.includes("hidden") === false) {
+        document.getElementById("contact-layer").classList.add("hidden")
+    }
+    if (document.getElementById("terms-layer").className.includes("hidden") === false) {
+        document.getElementById("terms-layer").classList.add("hidden");
+    }
+
+    let promo = document.getElementById("promotions-layer")
+    promo.className = promo.className.replace("hidden", "");
+}
+
+
+function closeNav(itemType) { //hides contact layer if open
+    if (itemType === "terms") {
+        let itemType = document.getElementById("terms-layer");
+        itemType.classList.add("hidden");
+    }
+
+    if (itemType === "contact") {
+        let itemType = document.getElementById("contact-layer");
+        itemType.classList.add("hidden");
+    }
+
+    if (itemType === "promo") { //hides promo layer if open
+        let itemType = document.getElementById("promotions-layer");
+        itemType.classList.add("hidden");
+    }
+    
+    if (itemType === "all") { //hides any displayed nav layer if open
+        document.getElementById("contact-layer").classList.add("hidden");
+        let promo = document.getElementById("promotions-layer").classList.add("hidden");
+        let terms = document.getElementById("terms-layer").classList.add("hidden");
+    }
+
+}
+
